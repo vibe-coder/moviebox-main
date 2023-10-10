a364d45ddfa8bbeaf345341ec3c97ec29404e1c4
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-10 10:23:45 +0100

    Imported home and footer component

--- moviebox/src/App.js
+++ moviebox/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Home/>
+      <Footer/>
     </div>
   );
 }
