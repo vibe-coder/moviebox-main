271974f8e27c9928508332b5bc0eb50f0d398694
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-10 11:01:21 +0100

    Added Links to pages

--- moviebox/src/components/Nav.jsx
+++ moviebox/src/components/Nav.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <nav className="w-100% h-20 flex gap-8 justify-between items-center">
-      <h1 className='text-white text-3xl font-bold m-0 flex-1 font-sans'>MovieBox</h1>
-      <input placeholder='Search' type='search' className='indent-2 bg-transparent border rounded font-sans text-white border-white h-8 w-60'/>
-      <button className='h-8 w-28 rounded text-white border border-blue-1 bg-blue-1 hover:border-metal'>Category</button>
-    </nav>
+    <>
+      <nav className="w-100% h-20 flex gap-8 justify-between items-center">
+        <Link to="/">
+          <h1 className='text-white text-3xl font-bold m-0 flex-1 font-sans'>
+            MovieBox
+          </h1>
+        </Link>
+        
+        <input placeholder='Search' type='search' className='indent-2 bg-transparent border rounded font-sans text-white border-white h-8 w-60'/>
+
+        <Link to="/category">
+          <button className='h-8 w-28 rounded text-white border border-blue-1 bg-blue-1 hover:border-metal'>
+            Category
+          </button>
+        </Link>
+      </nav>
+    </>
   )
 }
 
