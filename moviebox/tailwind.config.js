ba79222c54844b8024a73b3b70f6e9f25a98f44f
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-10 10:20:20 +0100

    Configured colours and font for TailwindCSS

--- moviebox/tailwind.config.js
+++ moviebox/tailwind.config.js
@@ -3,6 +3,25 @@ module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'blue-1': "#082f49",
+      'blue-2':'#0369a1'
+    },
+    
+    fontFamily: {
+      sans: ['Hind', 'sans-serif'],
+    },
   },
   plugins: [],
 }
