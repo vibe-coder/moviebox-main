4ec69b2f68ba0a914171e0e99c17fbdef301b070
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-10 11:01:06 +0100

    Configured Routes for pages

--- moviebox/src/App.js
+++ moviebox/src/App.js
@@ -1,15 +1,25 @@
+// import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
+import Category from './pages/category/Category';
 import Home from './pages/home/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Nav/>
-      <Home/>
-      <Footer/>
-    </div>
+    <>
+      <div className="App">
+        <Nav/>
+        <Routes>
+          <Route path='category' element={<Category/>} />
+          <Route path='/' element={<Home/>} />
+        </Routes>
+        <Footer/>
+      </div>
+
+
+    </>
   );
 }
 
