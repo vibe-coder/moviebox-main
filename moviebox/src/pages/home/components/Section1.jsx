6c9cd2cc31454a75150b7b0e078f3c7eaf27ac6d
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-11 14:39:49 +0100

    Added elemeents and styles section 1

--- moviebox/src/pages/home/components/Section1.jsx
+++ moviebox/src/pages/home/components/Section1.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import bgPoster from "../../../bg.jpg"
 
 function Section1() {
   return (
-    <section className='h-screen bg-metal'>Section1</section>
+    <section className='h-[85vh] bg-metal flex items-center  relative'>
+      
+      <img src={bgPoster} alt='background' className='absolute w-screen h-full object-cover ob'/>
+      
+      {/* Information Wrapper */}
+      
+      <div className='flex h-full w-full items-center px-16 z-[1] bg-gradient-to-t from-black to-transparent'>
+        <div>
+          {/* Title */}
+          <h1 className='text-white font-bold font-sans text-6xl m-0'>CHARLIE'S ANGELS</h1>
+          {/* Description */}
+          <p className='font-sans text-xl m-0 text-white'>Action, Adventure, Comedy | 15 November 2019 (USA)</p>
+          {/* Rating */}
+          <div className='my-4 text-white'>4.0</div>
+          {/* Button Wrapper */}
+          <div className='flex gap-4'>
+            <button className='h-8 w-28 rounded text-clack border border-yellow-1 bg-yellow-1 hover:border-white'>Watch Trailer</button>
+            <button className='h-8 w-28 rounded text-white border-2 border-white bg-transparent hover:border-blue-1'>View Info</button>
+            <button className='h-8 rounded text-white bg-transparent hover:border-blue-1'>+ Favorites</button>
+            <button className='h-8 rounded text-white bg-transparent'><i class="las la-ellipsis-v"></i></button>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
