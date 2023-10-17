9150cbf4d38f49af4878e2f660c47d459879f9b4
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-17 21:06:33 +0100

    Added elemets to footer

--- moviebox/src/components/Footer.jsx
+++ moviebox/src/components/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='h-80 bg-silver'>Footer</footer>
+    <footer className='h-80 bg-neutral-2 flex-col justify-between items-center'>
+      <h1 className='text-white text-3xl font-bold m-0 font-sans text-center'>
+        MovieBox
+      </h1>
+
+      {/* Socials */}
+      <div className='text-center'>Socials</div>
+
+      {/* Credits */}
+      <div className='text-center'>
+        <p>Credit: Designed-by: Akhil Akhi. Developed-by: Vibe-Coder </p>
+        <p>Developed with: TMDB API</p>
+
+      </div>
+    </footer>
   )
 }
 
