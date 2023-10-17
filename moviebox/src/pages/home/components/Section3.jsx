f0188e78fb223a7ff2daf106378edca13a5cfe88
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-17 21:12:18 +0100

    Added elemets

--- moviebox/src/pages/home/components/Section3.jsx
+++ moviebox/src/pages/home/components/Section3.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import bgPoster from "../../../bg.jpg"
 
 function Section3() {
   return (
-    <section className='h-screen bg-tahiti'>Section3</section>
+    <section className='bg-black px-16 pb-16'>
+      {/* ----------------------- Category Button Selection ------------------- */}
+      <div className='flex border-b border-neutral mb-16'>
+        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>Latest Trailers</button>
+        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>Coming Soon</button>
+        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>In Thearters</button>
+        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>Realease Calendar</button>
+        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>Box Office</button>
+        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>Showtimes and Tickets</button>
+        <div className=' flex-1 flex justify-end items-center'><button className='h-8 w-28 rounded font-sans text-yellow-1'>View All</button></div>
+      </div>
+
+
+      {/* ----------------------- Movie Category --------------------------- */}
+      <div className='flex justify-between gap-16 mb-6'>
+
+        {/* Single movie card */}
+        <div className=' h-[29rem] flex-col justify-between flex-1 '>
+          <div className='w-full h-[90%] bg-purple rounded-lg'>
+            <img className='h-full w-full object-cover object-center rounded-lg' src={bgPoster} alt='backgroundPoster' />
+          </div>
+          <div className='h-[10%] flex items-center text-white font-sans'><p>Avengers: Endgame</p></div>
+        </div>
+
+        {/* Single movie card */}
+        <div className=' h-[29rem] flex-col justify-between flex-1'>
+          <div className='w-full h-[90%] bg-purple rounded-lg'>
+            <img className='h-full w-full object-cover object-center rounded-lg' src={bgPoster} alt='backgroundPoster' />
+          </div>
+          <div className='h-[10%] flex items-center text-white font-sans'><p>Avengers: Endgame</p></div>
+        </div>
+
+        {/* Single movie card */}
+        <div className=' h-[29rem] flex-col justify-between flex-1'>
+          <div className='w-full h-[90%] bg-purple rounded-lg'>
+            <img className='h-full w-full object-cover object-center rounded-lg' src={bgPoster} alt='backgroundPoster' />
+          </div>
+          <div className='h-[10%] flex items-center text-white font-sans'><p>Avengers: Endgame</p></div>
+        </div>
+
+        {/* Single movie card */}
+        <div className=' h-[29rem] flex-col justify-between flex-1'>
+          <div className='w-full h-[90%] bg-purple rounded-lg'>
+            <img className='h-full w-full object-cover object-center rounded-lg' src={bgPoster} alt='backgroundPoster' />
+          </div>
+          <div className='h-[10%] flex items-center text-white font-sans'><p>Avengers: Endgame</p></div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
