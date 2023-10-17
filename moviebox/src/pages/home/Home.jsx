d09d272f54277932bc9138328de7b1de7ea9ab2e
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-17 21:08:54 +0100

    Removed Section5

--- moviebox/src/pages/home/Home.jsx
+++ moviebox/src/pages/home/Home.jsx
@@ -3,7 +3,6 @@ import Section1 from './components/Section1'
 import Section2 from './components/Section2'
 import Section3 from './components/Section3'
 import Section4 from './components/Section4'
-import Section5 from './components/Section5'
 
 function Home() {
   return (
@@ -12,7 +11,6 @@ function Home() {
       <Section2/>
       <Section3/>
       <Section4/>
-      <Section5/>
     </section>
   )
 }
