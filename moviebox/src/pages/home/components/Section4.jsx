e7321a66b687f08fee5f18a994fdbbf82ad968cc
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-18 13:55:44 +0100

    Adjusted buttons and added placeholder image

--- moviebox/src/pages/home/components/Section4.jsx
+++ moviebox/src/pages/home/components/Section4.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import bgPoster from "../../../bg.jpg"
 
 function Section4() {
   return (
     <section className='bg-black px-16 pb-32 '>
       {/* ----------------------- Category Button Selection ------------------- */}
       <div className='flex border-b border-neutral mb-12'>
-        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>Celebs</button>
-        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>Events</button>
-        <button className='h-16 px-3 font-sans text-white border-b-2 border-transparent hover:border-yellow-1 z-10'>News</button>
+        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Celebs</button>
+        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Events</button>
+        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>News</button>
         <div className=' flex-1 flex justify-end items-center'><button className='h-8 w-28 rounded font-sans text-yellow-1'>View All</button></div>
       </div>
 
@@ -17,7 +18,9 @@ function Section4() {
         {/* Single Celeb Container*/}
         <div className='flex col-soan-1'>
           {/* Picture */}
-          <div className='w-[1000px] h-[260px] bg-metal'>Picture</div>
+          <div className='w-[1000px] h-[260px] bg-metal'>
+            <img className='h-full cursor-pointer w-full object-cover object-center' src={bgPoster} alt='backgroundPoster' />
+          </div>
           {/* Info */}
           <div className=' ml-6'>
             <h1 className='text-yellow-1 font-bold text-lg tracking-widest font-sans mb-2'>Naomi Scott</h1>
@@ -33,7 +36,9 @@ function Section4() {
         {/* Single Celeb Container*/}
         <div className='flex col-soan-1'>
           {/* Picture */}
-          <div className='w-[1000px] h-[260px] bg-metal'>Picture</div>
+          <div className='w-[1000px] h-[260px] bg-metal'>
+            <img className='h-full cursor-pointer w-full object-cover object-center' src={bgPoster} alt='backgroundPoster' />
+          </div>
           {/* Info */}
           <div className=' ml-6'>
             <h1 className='text-yellow-1 font-bold text-lg tracking-widest font-sans mb-2'>Naomi Scott</h1>
