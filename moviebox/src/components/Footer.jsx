32e1f74f1710e984e1d9428f8582cad3d1dc3d32
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-18 19:02:26 +0100

    Added elemets to footer

--- moviebox/src/components/Footer.jsx
+++ moviebox/src/components/Footer.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 
+
 function Footer() {
   return (
-    <footer className='h-80 bg-neutral-2 flex-col justify-between items-center'>
-      <h1 className='text-white text-3xl font-bold m-0 font-sans text-center'>
-        MovieBox
-      </h1>
+    <footer className='h-80 bg-neutral-2 flex justify-center items-center'>
+      <div>
+        <h1 className='text-white m-0 mb-9 text-5xl font-bold text-center font-sans focus:outline-none focus-visible:ring'>
+          MOVIE<span className='font-hairline text-yellow-1'>BOX</span>
+        </h1>
 
-      {/* Socials */}
-      <div className='text-center'>Socials</div>
+        {/* Socials */}
+        <div className='text-center mb-12 flex gap-5 justify-center'>
+          <a href="/"><i className="lab la-facebook-f text-white"></i></a>
+          <a href="/"><i className="lab la-twitter text-white"></i></a>
+          <a href="/"><i className="lab la-pinterest-p text-white"></i></a>
+          <a href="/"><i className="lab la-instagram text-white"></i></a>
+          <a href="/"><i className="lab la-youtube text-white"></i></a>
+          
+        </div>
 
-      {/* Credits */}
-      <div className='text-center'>
-        <p>Credit: Designed-by: Akhil Akhi. Developed-by: Vibe-Coder </p>
-        <p>Developed with: TMDB API</p>
-
+        {/* Credits */}
+        <div className='text-center'>
+          <p className='text-white font-sans text-sm'>Credit: Designed-by: Akhil Akhi. Developed-by: Vibe-Coder </p>
+          <p className='text-white font-sans text-sm'>Developed with: TMDB API</p>
+        </div>
       </div>
     </footer>
   )
