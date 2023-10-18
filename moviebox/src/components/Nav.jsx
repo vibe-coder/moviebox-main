e9514e7df1836d6bc84af0701771720a6e864f7a
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-10-18 19:03:06 +0100

    Changes class to className

--- moviebox/src/components/Nav.jsx
+++ moviebox/src/components/Nav.jsx
@@ -15,7 +15,7 @@ function Nav() {
           {/* <span class="sr-only">Search</span> */}
           <input class="placeholder:italic placeholder:text-slate-400 block bg-transparent text-white w-full border border-slate-300 rounded-md py-2 pl-4 pr-12 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm focus:bg-transparentWhite hover:bg-transparentWhite" placeholder="Search for anything..." type="text" name="search"/>
           <span class="absolute inset-y-0 right-0 flex items-center pr-4">
-            <i class="las la-search 64px text-white scale-x-[-1]"></i>
+            <i className="las la-search 64px text-white scale-x-[-1]"></i>
           </span>
         </label>
 
