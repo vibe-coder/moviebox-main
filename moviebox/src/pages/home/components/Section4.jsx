edd51498bcb4cd8f2053dacb6e12049806f2017f
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-06 11:48:47 +0100

    Configured Section 4 for mobile view

--- moviebox/src/pages/home/components/Section4.jsx
+++ moviebox/src/pages/home/components/Section4.jsx
@@ -3,7 +3,7 @@ import bgPoster from "../../../bg.jpg"
 
 function Section4() {
   return (
-    <section className='bg-black px-16 pb-32 '>
+    <section className='bg-black px-6 pb-32 '>
       {/* ----------------------- Category Button Selection ------------------- */}
       <div className='flex border-b border-neutral mb-12'>
         <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Celebs</button>
@@ -13,19 +13,19 @@ function Section4() {
       </div>
 
       {/* Celeb Container */}
-      <div className='grid grid-cols-2 gap-6'>
+      <div className='grid grid-cols-1 gap-6'>
 
         {/* Single Celeb Container*/}
         <div className='flex col-soan-1'>
           {/* Picture */}
-          <div className='w-[1000px] h-[260px] bg-metal'>
+          <div className='w-[1000px] h-[180px] bg-metal'>
             <img className='h-full cursor-pointer w-full object-cover object-center' src={bgPoster} alt='backgroundPoster' />
           </div>
           {/* Info */}
-          <div className=' ml-6'>
+          <div className=' ml-6 overflow-hidden'>
             <h1 className='text-yellow-1 font-bold text-lg tracking-widest font-sans mb-2'>Naomi Scott</h1>
             <h2 className='font-sans mb-2 text-neutral-400'>Actress | <span className='text-yellow-1'>Aladdin</span></h2>
-            <p className='font-sans mb-4 text-neutral-300 font-hairline'>Naomi Scott is a multi-talented actor, singer and songwriter. She stars as "Jasmine"
+            <p className='font-sans mb-4 text-neutral-300 font-hairline text-ellipsis whitespace-pre-wrap overflow-hidden line-clamp-3'>Naomi Scott is a multi-talented actor, singer and songwriter. She stars as "Jasmine"
               is Disney's blockbuster live action remake Aladdin (2019), alongside Mena Massoud and Will Smith,
               directed by Guy Ritchie. Disney released the film in May 2019, and it became one of the biggest film of the year.
             </p>
@@ -36,20 +36,40 @@ function Section4() {
         {/* Single Celeb Container*/}
         <div className='flex col-soan-1'>
           {/* Picture */}
-          <div className='w-[1000px] h-[260px] bg-metal'>
+          <div className='w-[1000px] h-[180px] bg-metal'>
             <img className='h-full cursor-pointer w-full object-cover object-center' src={bgPoster} alt='backgroundPoster' />
           </div>
           {/* Info */}
-          <div className=' ml-6'>
+          <div className=' ml-6 overflow-hidden'>
             <h1 className='text-yellow-1 font-bold text-lg tracking-widest font-sans mb-2'>Naomi Scott</h1>
             <h2 className='font-sans mb-2 text-neutral-400'>Actress | <span className='text-yellow-1'>Aladdin</span></h2>
-            <p className='font-sans mb-4 text-neutral-300'>Naomi Scott is a multi-talented actor, singer and songwriter. She stars as "Jasmine"
+            <p className='font-sans mb-4 text-neutral-300 font-hairline text-ellipsis whitespace-pre-wrap overflow-hidden line-clamp-3'>Naomi Scott is a multi-talented actor, singer and songwriter. She stars as "Jasmine"
               is Disney's blockbuster live action remake Aladdin (2019), alongside Mena Massoud and Will Smith,
               directed by Guy Ritchie. Disney released the film in May 2019, and it became one of the biggest film of the year.
             </p>
             <button className='text-neutral-400 border-b-2 border-neutral-400 hover:text-white'>More</button>
           </div>
         </div>
+
+        {/* Single Celeb Container*/}
+        <div className='flex col-soan-1'>
+          {/* Picture */}
+          <div className='w-[1000px] h-[180px] bg-metal'>
+            <img className='h-full cursor-pointer w-full object-cover object-center' src={bgPoster} alt='backgroundPoster' />
+          </div>
+          {/* Info */}
+          <div className=' ml-6 overflow-hidden'>
+            <h1 className='text-yellow-1 font-bold text-lg tracking-widest font-sans mb-2'>Naomi Scott</h1>
+            <h2 className='font-sans mb-2 text-neutral-400'>Actress | <span className='text-yellow-1'>Aladdin</span></h2>
+            <p className='font-sans mb-4 text-neutral-300 font-hairline text-ellipsis whitespace-pre-wrap overflow-hidden line-clamp-3'>Naomi Scott is a multi-talented actor, singer and songwriter. She stars as "Jasmine"
+              is Disney's blockbuster live action remake Aladdin (2019), alongside Mena Massoud and Will Smith,
+              directed by Guy Ritchie. Disney released the film in May 2019, and it became one of the biggest film of the year.
+            </p>
+            <button className='text-neutral-400 border-b-2 border-neutral-400 hover:text-white'>More</button>
+          </div>
+        </div>
+
+
 
         
 
