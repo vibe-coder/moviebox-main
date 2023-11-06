22b4805a258c13f07412bbe8ab104e1bfcdb2656
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-06 12:14:37 +0100

    Confugred Navbar for Tablet view

--- moviebox/src/components/Nav.jsx
+++ moviebox/src/components/Nav.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 function Nav() {
   return (
     <>
-      <nav className="w-100% h-28 flex gap-4 justify-between absolute top-0 left-0 right-0 items-center px-6 z-10">
+      <nav className="w-100% h-28 flex  gap-4 justify-between absolute top-0 left-0 right-0 items-center px-6 z-10 md:gap-8 ">
         <Link className='' to="/">
-          <h1 className='text-white text-xl font-bold m-0 flex-1 font-sans focus:outline-none focus-visible:ring'>
+          <h1 className='text-white text-xl font-bold m-0 flex-1 font-sans focus:outline-none focus-visible:ring md:text-4xl'>
             MOVIE<span className='font-hairline text-yellow-1'>BOX</span>
           </h1>
         </Link>
         
-        <label className="relative block w-full">
+        <label className="relative block w-full md:w-50">
           {/* <span class="sr-only">Search</span> */}
           <input className="placeholder:italic h-10 placeholder:text-slate-400 block text-white w-full bg-transparentWhite border-slate-300 py-2 pl-4 pr-12 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm focus:bg-transparentWhite hover:bg-transparentWhite" placeholder="Search for anything..." type="text" name="search"/>
           <span className="absolute inset-y-0 right-0 flex items-center pr-4">
@@ -20,7 +20,7 @@ function Nav() {
         </label>
 
         <Link to="/category">
-          <button className='h-10 w-20 rounded text-black font-bold border border-yellow-1 bg-yellow-1 hover:bg-yellow-2 hover:border-yellow-2'>
+          <button className='h-10 w-20 rounded text-black font-bold border border-yellow-1 bg-yellow-1 hover:bg-yellow-2 hover:border-yellow-2 md:w-32'>
             Category
           </button>
         </Link>
