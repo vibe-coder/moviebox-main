a79c29ffebbad684fb67e1dc51ac8aa9d7216e6a
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-06 21:11:14 +0100

    Configured Section 2 for large screens

--- moviebox/src/pages/home/components/Section2.jsx
+++ moviebox/src/pages/home/components/Section2.jsx
@@ -8,9 +8,9 @@ function Section2() {
 
       {/* ----------------------- Category Button Selection ------------------- */}
       <div className='flex border-b border-neutral mb-10 overflow-scroll no-scrollbar'>
-        <div className='flex'>
+        <div className='flex lg:justify-between'>
           {/* Category button */}
-          <div className='flex gap-2 w-[200vw] md:gap-6 md:w-[140vw] '>
+          <div className='flex gap-2 w-[200vw] md:gap-6 md:w-[140vw] lg:w-full '>
             <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10 md:text-lg'>Movies</button>
             <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10 md:text-lg'>TV Series</button>
             <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10 md:text-lg'>Popular Downloads</button>
@@ -20,71 +20,71 @@ function Section2() {
           </div>
 
           {/* View All selection */}
-          <div className='flex justify-end items-center'><button className='pb-12 h-6 w-28 font-sans text-yellow-1 border-b-2 border-transparent hover:border-yellow-1 md:text-lg'>View All</button></div>
+          <div className='flex justify-end items-center lg:flex-1 lg:w-[600px]'><button className='pb-12 h-6 w-28 font-sans text-yellow-1 border-b-2 border-transparent hover:border-yellow-1 md:text-lg'>View All</button></div>
         </div>
       </div>
 
       {/* ----------------------- Movie Category --------------------------- */}
-      <div className='grid grid-cols-2 gap-x-4 gap-y-6 mb-12 md:grid-cols-3'>
+      <div className='grid grid-cols-2 gap-x-4 gap-y-6 mb-12 md:grid-cols-3 lg:grid-cols-6'>
         {/* Single movie card */}
-        <div className=' h-[15rem] w-[100%] flex-col justify-between'>
-          <div className='w-full h-[90%] bg-black rounded-lg'>
-            <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
+        <div className=' h-[15rem] w-[100%] flex-col justify-between lg:h-[20rem]'>
+          <div className='w-full h-[90%] bg-black'>
+            <img className='h-full cursor-pointer w-full object-cover object-center duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
           </div>
           <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans '><p className='md:text-lg'>Avengers: Endgame</p></div>
         </div>
 
-        <div className=' h-[15rem] w-[100%] flex-col justify-between'>
-          <div className='w-full h-[90%] bg-black rounded-lg'>
-            <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
+        <div className=' h-[15rem] w-[100%] flex-col justify-between lg:h-[20rem]'>
+          <div className='w-full h-[90%] bg-black'>
+            <img className='h-full cursor-pointer w-full object-cover object-center duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
           </div>
           <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans '><p className='md:text-lg'>Avengers: Endgame</p></div>
         </div>
 
-        <div className=' h-[15rem] w-[100%] flex-col justify-between'>
-          <div className='w-full h-[90%] bg-black rounded-lg'>
-            <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
+        <div className=' h-[15rem] w-[100%] flex-col justify-between lg:h-[20rem]'>
+          <div className='w-full h-[90%] bg-black'>
+            <img className='h-full cursor-pointer w-full object-cover object-center duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
           </div>
           <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans '><p className='md:text-lg'>Avengers: Endgame</p></div>
         </div>
 
-        <div className=' h-[15rem] w-[100%] flex-col justify-between'>
-          <div className='w-full h-[90%] bg-black rounded-lg'>
-            <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
+        <div className=' h-[15rem] w-[100%] flex-col justify-between lg:h-[20rem]'>
+          <div className='w-full h-[90%] bg-black'>
+            <img className='h-full cursor-pointer w-full object-cover object-center duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
           </div>
           <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans '><p className='md:text-lg'>Avengers: Endgame</p></div>
         </div>
 
-        <div className=' h-[15rem] w-[100%] flex-col justify-between'>
-          <div className='w-full h-[90%] bg-black rounded-lg'>
-            <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
+        <div className=' h-[15rem] w-[100%] flex-col justify-between lg:h-[20rem]'>
+          <div className='w-full h-[90%] bg-black'>
+            <img className='h-full cursor-pointer w-full object-cover object-center duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
           </div>
           <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans '><p className='md:text-lg'>Avengers: Endgame</p></div>
         </div>
 
-        <div className=' h-[15rem] w-[100%] flex-col justify-between'>
-          <div className='w-full h-[90%] bg-black rounded-lg'>
-            <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
+        <div className=' h-[15rem] w-[100%] flex-col justify-between lg:h-[20rem]'>
+          <div className='w-full h-[90%] bg-black'>
+            <img className='h-full cursor-pointer w-full object-cover object-center duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
           </div>
           <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans '><p className='md:text-lg'>Avengers: Endgame</p></div>
         </div>
       </div>
 
       {/* --------------------- Movie Banner Description ---------------------*/}
-      <div id='movieBanner' className='flex flex-col gap-3 md:flex-col'>
+      <div id='movieBanner' className='flex flex-col gap-3 md:flex-col lg:flex-row'>
 
         {/* Left Section */}
-        <div className='w-full relative rounded-lg h-[300px] md:h-[400px]'>
+        <div className='sm:w-full relative rounded-lg h-[300px] md:h-[400px] lg:w-[75%] lg:flex-2'>
           <img src={bgPoster} alt="bacground poster" className='object-cover object-center w-full h-full absolute rounded-lg z-[1]'/>
 
           <div className='absolute bg-transparetNeutral w-full h-full flex items-center py-6 pl-6 z-[2]'>
             <div>
               {/* Title */}
-              <h1 className='text-white font-bold font-sans text-3xl m-0 md:text-4xl'>CHARLIE'S ANGELS</h1>
+              <h1 className='text-white font-bold font-sans text-3xl m-0 md:text-4xl lg:text-5xl'>CHARLIE'S ANGELS</h1>
               {/* Description */}
-              <p className='font-sans text-sm m-0 text-white md:text-[16px]'>Action, Adventure, Comedy | 15 November 2019 (USA)</p>
+              <p className='font-sans text-sm m-0 text-white md:text-[16px] lg:text-xl'>Action, Adventure, Comedy | 15 November 2019 (USA)</p>
               {/* Rating */}
-              <div className='mt-4 text-white'><div className='border-2 border-yellow-1 w-12 rounded flex justify-center items-center'>4.0</div></div>
+              <div className='mt-4 text-white'><div className='border-2 border-yellow-1 w-12 rounded flex justify-center items-center lg:h-9 lg:w-14'>4.0</div></div>
               {/* Button Wrapper */}
               <div className='flex gap-4 flex-wrap'>
                 {/* <button className='h-8 w-28 rounded text-black font-medium border border-yellow-1 bg-yellow-1 hover:bg-yellow-2 hover:border-yellow-2'>Watch Trailer</button>
@@ -97,7 +97,7 @@ function Section2() {
         </div>
 
         {/* Right Section */}
-        <div id='rightSection' className='flex-[1] w-full text-white bg-midnigh border border-neutral-2 rounded-md bg-gradient-to-t from-neutral to-transparent md:w-[50%]'>
+        <div id='rightSection' className='flex-[1] w-full text-white bg-midnigh border border-neutral-2 rounded-md bg-gradient-to-t from-neutral to-transparent md:w-[50%] lg:flex-1'>
           <div className='p-6'>
             <p className='font-bold font-sans text-yellow-1 md:text-xl'>Suggestions:</p>
             <ul className='mt-4 pl-6'>
