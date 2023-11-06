97ee2e6222ca8c6ee4da51052a0c22aa3c883fdd
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-06 10:18:16 +0100

    Configured Section 2 to mobile view

--- moviebox/src/pages/home/components/Section2.jsx
+++ moviebox/src/pages/home/components/Section2.jsx
@@ -4,78 +4,66 @@ import "./scss/Section-2.scss"
 
 function Section2() {
   return (
-    <section className='bg-black p-16'>
+    <section className='p-6'>
 
       {/* ----------------------- Category Button Selection ------------------- */}
-      <div className='flex border-b border-neutral mb-16'>
-        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Movies</button>
-        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>TV Series</button>
-        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Popular Downloads</button>
-        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Top Rated Movies</button>
-        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Top Rated Series</button>
-        <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Premium</button>
-        <div className=' flex-1 flex justify-end items-center'><button className='pb-12 h-6 w-28 font-sans text-yellow-1 border-b-2 border-transparent hover:border-yellow-1'>View All</button></div>
-      </div>
+      <div className='flex border-b border-neutral mb-10 overflow-scroll no-scrollbar'>
+        <div className='flex'>
+          {/* Category button */}
+          <div className='flex gap-2 w-[200vw]'>
+            <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Movies</button>
+            <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>TV Series</button>
+            <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Popular Downloads</button>
+            <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Top Rated Movies</button>
+            <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Top Rated Series</button>
+            <button className='px-3 pb-12 h-6 font-sans text-white border-b-2  border-transparent hover:border-yellow-1 z-10'>Premium</button>
+          </div>
 
+          {/* View All selection */}
+          <div className='flex justify-end items-center'><button className='pb-12 h-6 w-28 font-sans text-yellow-1 border-b-2 border-transparent hover:border-yellow-1'>View All</button></div>
+        </div>
+      </div>
 
       {/* ----------------------- Movie Category --------------------------- */}
-      <div className='flex justify-between gap-16 mb-6'>
-
+      <div className='grid grid-cols-2 gap-x-4 gap-y-6 mb-12'>
         {/* Single movie card */}
-        <div className=' h-[29rem] flex-col justify-between flex-1 '>
+        <div className=' h-[15rem] w-[100%] flex-col justify-between flex-1 '>
           <div className='w-full h-[90%] bg-black rounded-lg'>
             <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
           </div>
-          <div className='h-[10%] flex items-center text-white font-sans'><p>Avengers: Endgame</p></div>
+          <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans'><p>Avengers: Endgame</p></div>
         </div>
 
         {/* Single movie card */}
-        <div className=' h-[29rem] flex-col justify-between flex-1 '>
+        <div className=' h-[15rem] w-[100%] flex-col justify-between flex-1 '>
           <div className='w-full h-[90%] bg-black rounded-lg'>
             <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
           </div>
-          <div className='h-[10%] flex items-center text-white font-sans'><p>Avengers: Endgame</p></div>
-        </div>
-
-        {/* Single movie card */}
-        <div className=' h-[29rem] flex-col justify-between flex-1 '>
-          <div className='w-full h-[90%] bg-black rounded-lg'>
-            <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
-          </div>
-          <div className='h-[10%] flex items-center text-white font-sans'><p>Avengers: Endgame</p></div>
-        </div>
-
-        {/* Single movie card */}
-        <div className=' h-[29rem] flex-col justify-between flex-1 '>
-          <div className='w-full h-[90%] bg-black rounded-lg'>
-            <img className='h-full cursor-pointer w-full object-cover object-center rounded-lg duration-[0.2s] hover:opacity-[.7]' src={bgPoster} alt='backgroundPoster' />
-          </div>
-          <div className='h-[10%] flex items-center text-white font-sans'><p>Avengers: Endgame</p></div>
+          <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans'><p>Avengers: Endgame</p></div>
         </div>
       </div>
 
-
       {/* --------------------- Movie Banner Description ---------------------*/}
-      <div id='movieBanner' className='h-[30rem] flex gap-6'>
-        
+      <div id='movieBanner' className='flex flex-col gap-3'>
+
         {/* Left Section */}
-        <div className='bg-yellow-1 flex-[3] relative rounded-lg'>
+        <div className='bg-purple w-92 relative rounded-lg h-[300px]'>
           <img src={bgPoster} alt="bacground poster" className='object-cover object-center w-full h-full absolute rounded-lg z-[1]'/>
 
-          <div className='absolute z-[2] w-full h-full flex items-center pl-16'>
+          <div className='absolute bg-transparetNeutral w-full h-full flex items-end py-6 pl-6 z-[2]'>
             <div>
               {/* Title */}
-              <h1 className='text-white font-bold font-sans text-5xl m-0'>CHARLIE'S ANGELS</h1>
+              <h1 className='text-white font-bold font-sans text-3xl m-0'>CHARLIE'S ANGELS</h1>
               {/* Description */}
-              <p className='font-sans text-md m-0 text-white'>Action, Adventure, Comedy | 15 November 2019 (USA)</p>
+              <p className='font-sans text-sm m-0 text-white'>Action, Adventure, Comedy | 15 November 2019 (USA)</p>
               {/* Rating */}
-              <div className='my-4 text-white'><div className='border-2 border-yellow-1 w-12 rounded flex justify-center items-center'>4.0</div></div>
+              <div className='mt-4 text-white'><div className='border-2 border-yellow-1 w-12 rounded flex justify-center items-center'>4.0</div></div>
               {/* Button Wrapper */}
-              <div className='flex gap-4'>
-                <button className='h-8 w-28 rounded text-black font-medium border border-yellow-1 bg-yellow-1 hover:bg-yellow-2 hover:border-yellow-2'>Watch Trailer</button>
+              <div className='flex gap-4 flex-wrap'>
+                {/* <button className='h-8 w-28 rounded text-black font-medium border border-yellow-1 bg-yellow-1 hover:bg-yellow-2 hover:border-yellow-2'>Watch Trailer</button>
                 <button className='h-8 w-28 rounded text-white border-2 border-white bg-transparent hover:border-yellow-1'>View Info</button>
-                <button className='h-8 rounded text-white bg-transparent hover:text-yellow-1'>+ Favorites</button>
-                <button className='h-8 rounded text-white bg-transparent hover:text-yellow-1'><i className="las la-ellipsis-v"></i></button>
+                <button className='h-8 rounded text-white bg-transparent hover:text-yellow-1'>+ Favorites</button> */}
+                {/* <button className='h-8 rounded text-white bg-transparent hover:text-yellow-1'><i className="las la-ellipsis-v"></i></button> */}
               </div>
             </div>
           </div>
@@ -94,11 +82,11 @@ function Section2() {
             </ul>
           </div>
         </div>
+
+
       </div>
-
-
     </section>
   )
 }
 
-export default Section2
+export default Section2;
