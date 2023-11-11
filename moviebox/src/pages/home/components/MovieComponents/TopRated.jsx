4378417b8c84d625adfb6f3f5ba9ae39dd5c95f0
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-12 00:27:27 +0100

    Reduced title size, increased gap & poster height

--- moviebox/src/pages/home/components/MovieComponents/TopRated.jsx
+++ moviebox/src/pages/home/components/MovieComponents/TopRated.jsx
@@ -32,15 +32,15 @@ function TopRated() {
   let imgPref = "https://image.tmdb.org/t/p/original/"
 
   return (
-    <div className='grid grid-cols-2 gap-x-4 gap-y-6 md:grid-cols-3 lg:grid-cols-6 lg:gap-x-5'>
+    <div className='grid grid-cols-2 gap-x-4 gap-y-11 md:grid-cols-3 lg:grid-cols-6 lg:gap-x-5'>
       {/* Single movie card */}
       {movies.map((data) => {
         return(
-          <div className=' h-[15rem] w-[100%] flex-col justify-between lg:h-[17rem]'>
+          <div className=' h-[15rem] w-[100%] flex-col justify-between lg:h-[20rem]'>
             <div className='w-full h-[90%] bg-black'>
               <img className='h-full cursor-pointer w-full object-cover object-center duration-[0.2s] hover:opacity-[.7]' src={imgPref + data.poster_path} alt='backgroundPoster' />
             </div>
-            <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans '><p className='md:text-lg lg:text-md lg:text-[1.1rem]'>{data.original_title}</p></div>
+            <div className='h-[10%] mt-2 flex items-center text-neutral-300 font-sans '><p className='md:text-lg text-md lg:text-[1rem] text-ellipsis whitespace-pre-wrap overflow-hidden line-clamp-1'>{data.original_title}</p></div>
           </div>
         )
       })}
