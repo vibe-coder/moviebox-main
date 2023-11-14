d05534b4cbd0429755a266b41298ecac8ba459f0
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-14 21:56:10 +0100

    added image path data

--- moviebox/src/pages/category/ButtonData.js
+++ moviebox/src/pages/category/ButtonData.js
@@ -1,75 +1,93 @@
 const ButtonData = [
   {
     name: "Action",
-    id: 28
+    id: 28,
+    img:'action'
   },
   {
     name: "Adventure",
-    id: 12
+    id: 12,
+    img:'adventure'
   },
   {
     name: "Animation",
-    id: 16
+    id: 16,
+    img:'animation'
   },
   {
     name: "Comedy",
-    id: 35
+    id: 35,
+    img:'comedy'
   },
   {
     name: "Crime",
-    id: 80
+    id: 80,
+    img:'crime'
   },
   {
     name: "Documentary",
-    id: 99
+    id: 99,
+    img:'documentary'
   },
   {
     name: "Drama",
-    id: 18
+    id: 18,
+    img:'drama'
   },
   {
     name: "Family",
-    id: 10751
+    id: 10751,
+    img:'family'
   },
   {
     name: "Fantasy",
-    id: 14
+    id: 14,
+    img:'fantasy'
   },
   {
     name: "History",
-    id: 36
+    id: 36,
+    img:'history'
   },
   {
     name: "Horror",
-    id: 27
+    id: 27,
+    img:'horror'
   },
   {
     name: "Music",
-    id: 10402
+    id: 10402,
+    img:'music'
   },
   {
     name: "Mystery",
-    id: 9648
+    id: 9648,
+    img:'mystery'
   },
   {
     name: "Romance",
-    id: 10749
+    id: 10749,
+    img:'romance'
   },
   {
     name: "Science Fiction",
-    id: 878
+    id: 878,
+    img:'sciencefiction'
   },
   {
     name: "Thriller",
-    id: 53
+    id: 53,
+    img:'thriller'
   },
   {
     name: "War",
-    id: 10752
+    id: 10752,
+    img:'war'
   },
   {
     name: "Western",
-    id: 37
+    id: 37,
+    img:'western'
   },
 ]
 
