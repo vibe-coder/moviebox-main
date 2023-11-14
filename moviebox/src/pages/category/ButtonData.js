b9906d17e71dc98d17ef6e475820f4b91fe0b97f
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-14 21:59:58 +0100

    Included file format to image path data

--- moviebox/src/pages/category/ButtonData.js
+++ moviebox/src/pages/category/ButtonData.js
@@ -2,92 +2,92 @@ const ButtonData = [
   {
     name: "Action",
     id: 28,
-    img:'action'
+    img:'action.jpg'
   },
   {
     name: "Adventure",
     id: 12,
-    img:'adventure'
+    img:'adventure.jpg'
   },
   {
     name: "Animation",
     id: 16,
-    img:'animation'
+    img:'animation.jpg'
   },
   {
     name: "Comedy",
     id: 35,
-    img:'comedy'
+    img:'comedy.jpg'
   },
   {
     name: "Crime",
     id: 80,
-    img:'crime'
+    img:'crime.jpg'
   },
   {
     name: "Documentary",
     id: 99,
-    img:'documentary'
+    img:'documentary.jpg'
   },
   {
     name: "Drama",
     id: 18,
-    img:'drama'
+    img:'drama.jpg'
   },
   {
     name: "Family",
     id: 10751,
-    img:'family'
+    img:'family.jpg'
   },
   {
     name: "Fantasy",
     id: 14,
-    img:'fantasy'
+    img:'fantasy.jpg'
   },
   {
     name: "History",
     id: 36,
-    img:'history'
+    img:'history.jpg'
   },
   {
     name: "Horror",
     id: 27,
-    img:'horror'
+    img:'horror.jpg'
   },
   {
     name: "Music",
     id: 10402,
-    img:'music'
+    img:'music.jpg'
   },
   {
     name: "Mystery",
     id: 9648,
-    img:'mystery'
+    img:'mystery.jpg'
   },
   {
     name: "Romance",
     id: 10749,
-    img:'romance'
+    img:'romance.jpg'
   },
   {
     name: "Science Fiction",
     id: 878,
-    img:'sciencefiction'
+    img:'sciencefiction.jpg'
   },
   {
     name: "Thriller",
     id: 53,
-    img:'thriller'
+    img:'thriller.jpg'
   },
   {
     name: "War",
     id: 10752,
-    img:'war'
+    img:'war.jpg'
   },
   {
     name: "Western",
     id: 37,
-    img:'western'
+    img:'western.jpg'
   },
 ]
 
