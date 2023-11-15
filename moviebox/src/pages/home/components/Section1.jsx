fac5bb2d2d6d183d1534b333e85cc0692a8fef82
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-15 20:37:42 +0100

    Added Loading Spinner

--- moviebox/src/pages/home/components/Section1.jsx
+++ moviebox/src/pages/home/components/Section1.jsx
@@ -74,8 +74,8 @@ const Section1 = () => {
   let imgPref = "https://image.tmdb.org/t/p/original/"
 
   return (
-    <section className='slideshow h-scree overflow-hidden bg-black' > 
-      <div className={`slideshowSlider relative w-full h-full whitespace-nowrap overflow-scroll no-scrollbar`} >
+    <section className='slideshow relative h-screen overflow-hidden bg-black' > 
+      <div className="slideshowSlider z-10 relative w-full h-full whitespace-nowrap overflow-hidden no-scrollbar" >
         {movieData.map((data) => {
           return(
             <div key={data.id} style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }} className='relative w-full h-screen inline-block transition ease-in-out duration-[1000ms]'>
@@ -84,31 +84,43 @@ const Section1 = () => {
 
                 {/* Information Wrapper */}
                 <div className='flex absolute h-screen w-full items-center px-6 z-[2] md:px-16 lg:px-28 whitespace-normal bg-gradient-to-t from-black to-transparent'>
-              <div className='md:w-[80%] lg:w-[600PX]'>
-                {/* Title */}
-                <h1 className='text-white font-bold font-sans text-4xl m-0 md:text-7xl'>{data.original_title}</h1>
-                {/* Description */}
-                <p className='font-sans text-sm m-0 text-neutral-400 md:text-xl'>{data.genre_ids} | {data.release_date}</p>
-                {/* Overview */}
-                <p className='text-white text-sm font-sans mt-5 font-hairline md:text-lg '>{data.overview}</p>
-                {/* Rating */}
-                <div className='my-6 text-white'>
-                  <div className='border-2 border-yellow-1 w-12 rounded flex justify-center items-center md:h-9 md:w-14'>4.0</div>
-                </div>
-                {/* Button Wrapper */}
-                <div className='flex gap-4 flex-wrap'>
-                  <button className='transition ease-in-out h-8 w-28 rounded text-black font-medium border border-yellow-1 hover:-translate-y-1 bg-yellow-1 hover:bg-white hover:border-white duration-200 md:h-11 md:w-32'>Watch Trailer</button>
-                  <button className='transition ease-in-out h-8 w-28 rounded text-white border-2 border-white bg-transparent hover:border-yellow-1 hover:text-yellow-1 hover:-translate-y-1 duration-200 md:h-11 md:w-32'>View Info</button>
-                  <button className='transition ease-in-out h-8 rounded text-white bg-transparent hover:text-yellow-1 hover:-translate-y-1 duration-200 md:h-11'>+ Favorites</button>
-                  {/* <button className='h-8 rounded text-white bg-transparent hover:text-yellow-1'><i className="las la-ellipsis-v"></i></button> */}
-                </div>
-              </div>
+                  <div className='md:w-[80%] lg:w-[600PX]'>
+                    {/* Title */}
+                    <h1 className='text-white font-bold font-sans text-4xl m-0 md:text-7xl'>{data.original_title}</h1>
+                    {/* Description */}
+                    <p className='font-sans text-sm m-0 text-neutral-400 md:text-xl'>{data.genre_ids} | {data.release_date}</p>
+                    {/* Overview */}
+                    <p className='text-white text-sm font-sans mt-5 font-hairline md:text-lg '>{data.overview}</p>
+                    {/* Rating */}
+                    <div className='my-6 text-white'>
+                      <div className='border-2 border-yellow-1 w-12 rounded flex justify-center items-center md:h-9 md:w-14'>4.0</div>
+                    </div>
+                    {/* Button Wrapper */}
+                    <div className='flex gap-4 flex-wrap'>
+                      <button className='transition ease-in-out h-8 w-28 rounded text-black font-medium border border-yellow-1 hover:-translate-y-1 bg-yellow-1 hover:bg-white hover:border-white duration-200 md:h-11 md:w-32'>Watch Trailer</button>
+                      <button className='transition ease-in-out h-8 w-28 rounded text-white border-2 border-white bg-transparent hover:border-yellow-1 hover:text-yellow-1 hover:-translate-y-1 duration-200 md:h-11 md:w-32'>View Info</button>
+                      <button className='transition ease-in-out h-8 rounded text-white bg-transparent hover:text-yellow-1 hover:-translate-y-1 duration-200 md:h-11'>+ Favorites</button>
+                      {/* <button className='h-8 rounded text-white bg-transparent hover:text-yellow-1'><i className="las la-ellipsis-v"></i></button> */}
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           )
         })}
-      </div>   
+      </div> 
+
+      {/* Loader */}
+      <div className='bg-neutral-black absolute z-[1] top-0 left-0 right-0 bottom-0'>
+        {/* Spinner */}
+        <div role="status" className='h-screen w-screen flex justify-center items-center'>
+          <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-yellow-1" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+          </svg>
+          <span class="sr-only">Loading...</span>
+        </div>
+      </div>
     </section>
   )
 }
