4e9e2e4d3b559a5edead5aae6f45ee89fc749a1a
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-11-20 17:17:36 +0100

    Changed button data image src

--- moviebox/src/pages/category/ButtonData.js
+++ moviebox/src/pages/category/ButtonData.js
@@ -2,92 +2,92 @@ const ButtonData = [
   {
     name: "Action",
     id: 28,
-    img:'/action'
+    img:'./images/action.jpg'
   },
   {
     name: "Adventure",
     id: 12,
-    img:'/adventure'
+    img:'./images/adventure.jpg'
   },
   {
     name: "Animation",
     id: 16,
-    img:'/animation'
+    img:'./images/animation.jpg'
   },
   {
     name: "Comedy",
     id: 35,
-    img:'/comedy'
+    img:'./images/comedy.jpg'
   },
   {
     name: "Crime",
     id: 80,
-    img:'/crime'
+    img:'./images/crime.jpg'
   },
   {
     name: "Documentary",
     id: 99,
-    img:'/documentary'
+    img:'./images/documentary.jpg'
   },
   {
     name: "Drama",
     id: 18,
-    img:'/drama'
+    img:'./images/drama.jpg'
   },
   {
     name: "Family",
     id: 10751,
-    img:'/family'
+    img:'./images/family.jpg'
   },
   {
     name: "Fantasy",
-    id: 1/4,
-    img:'fantasy'
+    id: 14,
+    img:'./images/animation.jpg'
   },
   {
     name: "History",
     id: 36,
-    img:'/history'
+    img:'./images/history.jpg'
   },
   {
     name: "Horror",
     id: 27,
-    img:'/horror'
+    img:'./images/horror.jpg'
   },
   {
     name: "Music",
     id: 10402,
-    img:'/music'
+    img:'./images/music.jpg'
   },
   {
     name: "Mystery",
     id: 9648,
-    img:'/mystery'
+    img:'./images/mystery.jpg'
   },
   {
     name: "Romance",
     id: 10749,
-    img:'/romance'
+    img:'./images/romance.jpg'
   },
   {
     name: "Science Fiction",
     id: 878,
-    img:'/sciencefiction'
+    img:'./images/sciencefiction.jpg'
   },
   {
     name: "Thriller",
     id: 53,
-    img:'/thriller'
+    img:'./images/thriller.jpg'
   },
   {
     name: "War",
     id: 10752,
-    img:'/war'
+    img:'./images/war.jpg'
   },
   {
     name: "Western",
     id: 37,
-    img:'/western'
+    img:'./images/western.jpg'
   },
 ]
 
